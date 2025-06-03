2d1fe63161d4150a17c33cca7ba3b19e46a5cd52
JustinTWind <justin.20@outlook.com>
2025-06-03 11:04:48 -0500

    refactor se arreglaron las variables 1.0

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        byte edadPersona;
+        double precioProducto;
+        char generoPersona;
+        String matricula;
+        int productosDisponibles;
+        double distanciaKilometros;
+        float temperaturaAmbiente;
+        float velocidadAuto;
+        boolean esMayorEdad;
+        boolean puertaAbierta;
+        long poblacionMundial;
+        char letraExamen;
+        String palabra;
+        byte numeroEstrellas;
+        boolean archivoActivo;
+        double precioBillete;
+        long puntuacionVideojuego;
+        boolean tieneLicencia;
+        double calificacionPromedio;
+        String nombreCompleto;
+        int cantidadLibros;;
+        double tipoCambio;
+        String matriculaEstudiante;
+        double puntajePromedioDeportista;
+        char caracterEspecial;
+        double constantePlanck;
+        boolean sensorFuncionando;
+        LocalDateTime horaEvento;
+        boolean clienteRegistrado;
+        String identificadorCliente;
+        double volumenRecipiente;
+        String nombreUsuario;
+        String codigoProducto;
+        long visitantesSitioWeb;
+        boolean pagoConfirmado;
+        float pesoPersona;
+        float notaFinal;
+        char teclaPulsada;
+        String nombreEmpresa;
+    }
+}
